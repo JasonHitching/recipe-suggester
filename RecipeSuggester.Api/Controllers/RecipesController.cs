ab83b030d45d5a9280f752f5ead93f134bed7e23
Jason Hitching <jasonhitching1994@gmail.com>
2026-02-15 18:52:22 +0000

    Add Serilog logging and new POST endpoint to RecipesController
    Integrated Serilog for console and file logging. Updated RecipesController to use ILogger and added a POST endpoint for prompt-based chat requests with logging. Modified Program.cs for Serilog setup, added Serilog NuGet packages, and updated RecipeSuggester.Api.http with sample requests. Included log.txt with sample output.

--- RecipeSuggester.Api/Controllers/RecipesController.cs
+++ RecipeSuggester.Api/Controllers/RecipesController.cs
@@ -9,9 +9,10 @@ namespace RecipeSuggester.Api.Controllers;
 /// </summary>
 [ApiController]
 [Route("[controller]")]
-public class RecipesController(IChatClient ollamaChatClient) : ControllerBase
+public class RecipesController(IChatClient ollamaChatClient, ILogger<RecipesController> logger) : ControllerBase
 {
     private readonly IChatClient _ollamaChatClient = ollamaChatClient;
+    private readonly ILogger<RecipesController> _logger = logger;
 
     [HttpGet]
     [Route("/test")]
@@ -20,5 +21,24 @@ public class RecipesController(IChatClient ollamaChatClient) : ControllerBase
         var modelResponse = await _ollamaChatClient.GetResponseAsync("What is the meaning of life?");
 
         return modelResponse.ToString();
+    }
+
+    [HttpPost]
+    public async Task<string> TestApiChatResponse2(string prompt)
+    {
+        try
+        {
+            _logger.LogInformation("Making a request to ollama with the following prompt: {prompt}", prompt);
+            var promptResponse = await _ollamaChatClient.GetResponseAsync(prompt);
+
+            return promptResponse.ToString();
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Error occured whilst making chat client request {error}", ex);
+        }
+
+        return "";
     }
 }
--- RecipeSuggester.Api/Program.cs
+++ RecipeSuggester.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.AI;
 using OllamaSharp;
 using RecipeSuggester.Api.Data;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -9,7 +10,15 @@ builder.Services.AddDbContext<RecipeContext>();
 
 IChatClient ollamaClient = new OllamaApiClient("http://localhost:11434/", "smollm2:135m");
 
+builder.Host.UseSerilog((context, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .WriteTo.Console()
+        .WriteTo.File("log.txt");
+});
+
 builder.Services.AddSingleton<IChatClient>(ollamaClient);
+
 
 var app = builder.Build();
 
